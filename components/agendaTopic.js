1df77e9efdb4f070f2ebd273bd0b7a49a6a41675
Ryan Frantz <ryanleefrantz@gmail.com>
2022-10-03 16:48:55 -0400

    Tidy up agenda topic.
    - Moves it inside its own div.
    - The previous iteration, with a span above the topic, inside a
      fragment, created a bug where the nth-of-type CSS pseudo-selector
      would see the span as a sibling and shift the styling in unhelpful
      ways.

--- components/agendaTopic.js
+++ components/agendaTopic.js
@@ -2,11 +2,11 @@
 
 export default function AgendaTopic({time}) {
   return (
-    <>
+    <div>
       <span>{time}</span>
       <div className="agenda-topic" id={`topic_${time}`}
         contentEditable={true}>
       </div>
-      </>
+    </div>
   );
 }
