03a71f4611d9e084c57ea49b5db8e680e5121f4f
Ryan Frantz <ryanleefrantz@gmail.com>
2022-11-01 07:57:58 -0400

    Converts meetingStart into friendly date and time.

--- components/meetingLink.js
+++ components/meetingLink.js
@@ -1,14 +1,17 @@
+import { getYmd, getHhmm } from './utils.js';
 /*
 * Given a saved meeting's start time (ms since epoch) and the details
 * we save about the agenda and notes, return a link. The link will have
 * a handler attached that will load the meeting.
 */
+// meetingStart is ms since epoch.
 export default function MeetingLink({meetingStart, title}) {
+  console.log(meetingStart);
+  const d = new Date(Number.parseInt(meetingStart));
   return (
-    // TODO: Convert meetingStart into a friendly date and time and use in title.
     // TODO: Add a click handler that will load the given meeting.
     <p key={meetingStart} id="`meeting_${meetingStart}`">
-    {title}
+    {getYmd(d)} {getHhmm(d)} {title}
     </p>
   );
 }
